abb22a2531e6448d38f60bf59ddb064b4298d132
dg12988 <doug.goldberg@me.com>
2021-08-10 12:55:32 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import { fromEvent } from "rxjs";
+import { map } from "rxjs/operators";
 
 const sliderInput = document.querySelector('input#slider');
 
-fromEvent(sliderInput, 'input').subscribe(value => console.log(value));
+fromEvent(sliderInput, 'input').pipe(
+  map(event => event.target['value'])
+).subscribe(value => console.log(value));
