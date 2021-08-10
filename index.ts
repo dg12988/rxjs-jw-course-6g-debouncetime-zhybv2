7798250437d1c9de39912765758a7957ec625e96
dg12988 <doug.goldberg@me.com>
2021-08-10 12:57:24 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import { fromEvent } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, debounceTime } from "rxjs/operators";
 
 const sliderInput = document.querySelector('input#slider');
 
 fromEvent(sliderInput, 'input').pipe(
+  debounceTime(2000),
   map(event => event.target['value'])
 ).subscribe(value => console.log(value));
